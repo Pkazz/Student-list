f928cc16063d346001bff1ad69b448c19eff86e6
przem <przemyslawkazimierczak@gmail.com>
2023-06-29 00:24:43 +0200

    Zaciągnięcie plików źródłowych

---
+++ src/main/java/com/example/StudentList/StudentApi.java
@@ -0,0 +1,34 @@
+package com.example.StudentList;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentApi {
+
+    private List<Student> studentList;
+
+    public StudentApi() {
+        this.studentList = new ArrayList<>();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudents(@PathVariable int id) {
+        return studentList.stream()
+                .filter(student -> student.getId() == id).findFirst().get();
+    }
+
+    @GetMapping("/showStudentList")
+    public List<Student> getStudents() {
+        return studentList;
+    }
+
+    @PostMapping("/add")
+    public boolean addStudent(@RequestBody Student student) {
+        return studentList.add(student);
+    }
+
+}
